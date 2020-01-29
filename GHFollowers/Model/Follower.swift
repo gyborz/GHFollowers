0e5b9a57de4ee99bb45011081aa7945853c8c9a8
gyborz <borzgy95@gmail.com>
2020-01-29 19:03:03 +0100

    Add Follower and User structs, fix bugs with navigation controller

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Gyorgy Borz on 2020. 01. 29..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    let login: String
+    let avatarURL: String
+}
