1a3ededfae4c766b997aee5fac61e5e5f52713b5
gyborz <borzgy95@gmail.com>
2020-01-29 11:44:05 +0100

    Add custom Button and Textfield

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Gyorgy Borz on 2020. 01. 29..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {    // needed when using storyboards
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+}
