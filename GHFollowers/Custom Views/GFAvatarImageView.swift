39de3216a4518d1e33a7d8e222fd0638ea93d808
gyborz <borzgy95@gmail.com>
2020-01-29 21:32:09 +0100

    Add custom imageView and collection view cell

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Gyorgy Borz on 2020. 01. 29..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage!
+    }
+
+}
