b7d28ce15706c4abfacb10057b0d3acc3f35aacc
gyborz <borzgy95@gmail.com>
2020-01-31 13:36:04 +0100

    Add image downloading and caching

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -11,7 +11,8 @@ import UIKit
 class NetworkManager {
     
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString,UIImage>()
     
     private init() {}
     
