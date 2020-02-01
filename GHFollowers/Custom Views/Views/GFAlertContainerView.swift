aa4dfbcb7d22f29c0276a60b9d3faf195ffaa08a
gyborz <borzgy95@gmail.com>
2020-02-01 20:10:40 +0100

    Refactor image loading, add GFDataLoadingVC, refactor code accordingly

---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Gyorgy Borz on 2020. 02. 01..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
