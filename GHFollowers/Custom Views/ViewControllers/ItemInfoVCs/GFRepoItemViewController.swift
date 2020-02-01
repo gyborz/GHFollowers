2aaa1244cfbd5ebcaf2287213555077541574dbe
gyborz <borzgy95@gmail.com>
2020-02-01 14:50:55 +0100

    Add GFFollowerItemVC, GFItemInfoVC, GFRepoItemVC

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GFRepoItemViewController.swift
+//  GHFollowers
+//
+//  Created by Gyorgy Borz on 2020. 02. 01..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class GFRepoItemViewController: GFItemInfoViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+    
+}
--- GHFollowers/Screens/UserInfoViewController.swift
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -47,6 +47,8 @@ class UserInfoViewController: UIViewController {
             case .success(let user):
                 DispatchQueue.main.async {
                     self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                    self.add(childVC: GFRepoItemViewController(user: user), to: self.itemViewOne)
+                    self.add(childVC: GFFollowerItemViewController(user: user), to: self.itemViewTwo)
                 }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
@@ -68,9 +70,6 @@ class UserInfoViewController: UIViewController {
                 itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
             ])
         }
-        
-        itemViewOne.backgroundColor = .systemPink
-        itemViewTwo.backgroundColor = .systemBlue
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
